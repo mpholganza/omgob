248203caded8e66e67a42816a2d3cddb884e2752
Paolo Holganza <paolo.holganza@gmail.com>
2014-01-16 02:55:45 -0800

    add descriptions to temp fixtures

--- server/fixtures.js
+++ server/fixtures.js
@@ -1,16 +1,18 @@
 if (Food.find().count() === 0) {
   Food.insert({
     name: 'Peanut Butter Jelly Cheese',
-    price: '$4.00'
+    price: '$4.00',
+    description: 'A peanut butter and strawberry jam sandwich with a cheesy twist.'
   });
   
   Food.insert({
     name: 'Ham and Eggs',
-    price: '$7.00'
+    price: '$7.00',
+    description: 'Spam and eggs on rice.'
   });
 
   Food.insert({
     name: 'Blueberry Pudding Cake',
-    price: '$4.00'
+    price: '$4.00',
   });
 }
